4920a9aff9ffe238889087da96d02bb9eecb9d3e
Kapkov Kirill <kap-kirill@yandex.ru>
2018-11-06 21:40:20 +0300

    Cracking the Code Interview: Chapter 1 Arrays and Strings - Question 2 - Check Permutation

---
+++ src/main/java/ru/thecop/cdpr/ctci/ch1arraysandstrings/Ch1Q2CheckPermutation.java
@@ -0,0 +1,38 @@
+package ru.thecop.cdpr.ctci.ch1arraysandstrings;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ch1Q2CheckPermutation {
+//    Given two strings, write a method to decide if one is a permutation of the
+//    other.
+
+    public static boolean checkPermutationArraySort(String s1, String s2) {
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        char[] a1 = s1.toCharArray();
+        char[] a2 = s2.toCharArray();
+
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+
+        return Arrays.equals(a1,a2);
+    }
+
+    public static boolean checkPermutationMap(String s1, String s2) {
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s1.toCharArray()) {
+            map.put(c,map.computeIfAbsent(c,count -> 0)+1);
+        }
+        for (char c : s2.toCharArray()) {
+            map.put(c,map.computeIfAbsent(c,count -> 0)-1);
+        }
+        return map.values().stream().noneMatch(count->count!=0);
+    }
+
+}
---
+++ src/test/java/ru/thecop/cdpr/ctci/ch1arraysandstrings/Ch1Q2CheckPermutationTest.java
@@ -0,0 +1,25 @@
+package ru.thecop.cdpr.ctci.ch1arraysandstrings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Ch1Q2CheckPermutationTest {
+
+    @Test
+    void testCheckPermutationArraySort() {
+        assertTrue(Ch1Q2CheckPermutation.checkPermutationArraySort("abc","cab"));
+        assertTrue(Ch1Q2CheckPermutation.checkPermutationArraySort("aaaabbbb","abababab"));
+        assertFalse(Ch1Q2CheckPermutation.checkPermutationArraySort("abcd","caba"));
+        assertFalse(Ch1Q2CheckPermutation.checkPermutationArraySort("abcd","dcb"));
+    }
+
+    @Test
+    void testCheckPermutationMap() {
+        assertTrue(Ch1Q2CheckPermutation.checkPermutationMap("abc","cab"));
+        assertTrue(Ch1Q2CheckPermutation.checkPermutationMap("aaaabbbb","abababab"));
+        assertFalse(Ch1Q2CheckPermutation.checkPermutationMap("abcd","caba"));
+        assertFalse(Ch1Q2CheckPermutation.checkPermutationMap("abcd","dcb"));
+    }
+}
